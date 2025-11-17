6e31ddb23f89413cc60b51155d707c4d6a4bfe60
Sarthak Chaturvedi <alphabyte.founders@gmail.com>
2025-11-17 12:24:07 +0530

    Inline Netflix title logo asset

--- src/NetflixTitle.tsx
+++ src/NetflixTitle.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './NetflixTitle.css';
 import netflixSound from './netflix-sound.mp3';
 import { useNavigate } from 'react-router-dom';
-import logoImage from '../src/images/logo-2.png'; // Update with the path to your logo
+import { sankalpChaturvediLogo } from './images/sankalpChaturvediLogo';
 
 const NetflixTitle = () => {
   const [isClicked, setIsClicked] = useState(false);
@@ -25,8 +25,8 @@ const NetflixTitle = () => {
 
   return (
     <div className="netflix-container" onClick={handlePlaySound}>
-      <img 
-        src={logoImage} 
+      <img
+        src={sankalpChaturvediLogo}
         alt="Custom Logo" 
         className={`netflix-logo ${isClicked ? 'animate' : ''}`} 
       />
